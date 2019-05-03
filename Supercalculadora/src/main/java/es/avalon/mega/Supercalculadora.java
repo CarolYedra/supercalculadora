6ae71a1e6f999cdd4f6424d7b452812b8d47225e
Formacion <Formacion@DESKTOP-34UJAOH>
2019-05-03 09:10:52 +0200

    final

---
+++ Supercalculadora/src/main/java/es/avalon/mega/Supercalculadora.java
@@ -0,0 +1,11 @@
+package es.avalon.mega;
+
+import es.avalon.Calculadora;
+
+public class Supercalculadora {
+	
+	public static double sumarAlcuadrado(double a,double b) {
+		return Math.pow(Calculadora.testSuma(a,b),2);
+	}
+
+}
---
+++ Supercalculadora/src/test/java/es/avalon/mega/SupercalculadoraTest.java
@@ -0,0 +1,16 @@
+package es.avalon.mega;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SupercalculadoraTest {
+	
+	@Test
+	public void test() {
+		
+		assertEquals(Supercalculadora.sumarAlcuadrado(2, 2),16.0,0.0);
+	}
+	
+
+}
